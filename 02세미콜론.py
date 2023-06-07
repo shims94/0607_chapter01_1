11b51e98b73bb6df5f8e4fbe1d86a962a4d45f5d
shims94 <shims94@gmail.com>
2023-06-07 15:47:30 +0900

    Initial commit

---
+++ 02세미콜론.py
@@ -0,0 +1,8 @@
+# 기본문법
+# 세미콜론(;)
+
+print("Hello python")
+print("Hello python");
+# 한줄에 여러구문을 사용할때 세미콜론으로 구분해주는 경우는 있으나 줄 바꾸는걸 추천
+# shift + f6 파일명 변경 단축키
+# shift + f10 실행 단축키
---
+++ 04들여쓰기.py
@@ -0,0 +1,6 @@
+#get apple stock price
+a = 1
+if a == 10:
+    print("a는 10입니다") # if문 다음에 들여쓰기가 되어 있어야 작동
+else:
+     print("a는 10이아닙니다")
